aefab0a6507869cb44a0a36e4b0e11040d3e8a8b
tbor00 <btimoteo@outlook.com>
2021-06-01 12:09:35 -0300

    Done - example with package send-mailchimp-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Newsletter from "send-suscriptor-mailchimp-react"
 
-function App() {
+const App = () => {
+
+  const todo = {
+    formClassName: 'form-class',
+    inputEmailClassName:"input-classname",
+    inputSubmitClassName: "submit-classname",
+    valueSubmit : 'Suscribirme',
+    listMailchimp: 'https://frontendschool.us1.list-manage.com/subscribe/post?u=3a7bf9a165f57222b5ab10011&amp;id=52b6b1c244&',
+    messageSucces: 'Bien hecho!',
+    messageError: 'No se pudo completar',
+    messageClassName: "property-class"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Newsletter info={todo}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
